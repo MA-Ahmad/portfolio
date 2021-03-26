6bbed9ff41c2f6677dabf9b1cc70e9d86e008a24
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-26 13:19:01 -0700

    Create header.tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack
+} from "@chakra-ui/react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+
+import { ColorModeSwitcher } from "../ColorModeSwitcher";
+
+const Links = ["About", "Open Source", "Blog"];
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700")
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box
+        bg={useColorModeValue("gray.100", "gray.900")}
+        px={4}
+        boxShadow={"lg"}
+      >
+        <Flex
+          h={16}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          w={["90%", "85%", "80%"]}
+          maxW={750}
+          mx="auto"
+        >
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+            aria-label={"Open Menu"}
+            display={["inherit", "inherit", "none"]}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <Avatar
+                size={"sm"}
+                src={"https://avatars2.githubusercontent.com/u/37842853?v=4"}
+              />
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map(link => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <IconButton
+              as={Link}
+              href={"https://github.com/MA-Ahmad"}
+              size={"md"}
+              icon={<FaGithub />}
+              aria-label={"Github account"}
+            />
+            <ColorModeSwitcher justifySelf="flex-end" />
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} w={["90%", "85%", "80%"]}
+          maxW={750}
+          >
+            <Stack as={"nav"} spacing={4}>
+              {Links.map(link => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
