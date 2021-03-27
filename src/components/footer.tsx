ee515b275d51dd99122907098320b46b3d8e74f6
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-27 13:24:18 -0700

    Create footer.tsx

---
+++ src/components/footer.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import {
+  Stack,
+  IconButton,
+  Link,
+  Box,
+  Text,
+  useColorModeValue,
+  HStack
+} from "@chakra-ui/react";
+import siteConfig from "../configs/site-config";
+
+const iconProps = {
+  variant: "ghost",
+  size: "lg",
+  isRound: true
+};
+
+const Footer = () => {
+  return (
+    <Stack
+      as="footer"
+      isInline
+      spacing={[1, 2]}
+      p={4}
+      justifyContent="space-between"
+      alignItems="center"
+      w={["90%", "85%", "80%"]}
+      maxW={800}
+      mx="auto"
+    >
+      <HStack>
+        <Text fontSize="sm" color={useColorModeValue("gray.500", "gray.200")}>
+          © {new Date().getFullYear()} Muhammad Ahmad{" "}
+        </Text>
+        {/* <Box fontSize="md" textAlign="left">
+        Website built with
+        <Box
+          as="span"
+          mx="2"
+          _before={{
+            cursor: "default",
+            content: '"❤️"'
+          }}
+          _hover={{
+            _before: {
+              content: '"☕️"'
+            }
+          }}
+        />
+        in Pakistan{"  "}🇵🇰
+      </Box> */}
+      </HStack>
+      <Box>
+        {siteConfig.author.accounts.map(sc => (
+          <IconButton
+            as={Link}
+            isExternal
+            href={sc.url}
+            aria-label={sc.label}
+            size="lg"
+            colorScheme={sc.type}
+            icon={sc.icon}
+            {...iconProps}
+          />
+        ))}
+      </Box>
+    </Stack>
+  );
+};
+
+export default Footer;
