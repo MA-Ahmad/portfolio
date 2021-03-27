5e4f3bfc2638435292cdf9d578a25557147b6e54
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-27 03:24:52 -0700

    Update ColorModeSwitcher.tsx

--- src/ColorModeSwitcher.tsx
+++ src/ColorModeSwitcher.tsx
@@ -1,30 +1,53 @@
-import * as React from "react"
+import * as React from "react";
 import {
   useColorMode,
   useColorModeValue,
   IconButton,
   IconButtonProps,
-} from "@chakra-ui/react"
-import { FaMoon, FaSun } from "react-icons/fa"
+  Tooltip
+} from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import useSound from "use-sound";
 
-type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">
+type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">;
 
-export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
-  const { toggleColorMode } = useColorMode()
-  const text = useColorModeValue("dark", "light")
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
+export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = props => {
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue("dark", "light");
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  const [play] = useSound("/lightswitch.mp3", {
+    volume: 0.05,
+    sprite: {
+      on: [0, 300],
+      off: [500, 300]
+    }
+  });
+
+  const handleClick = () => {
+    text === "dark" ? play({ id: "on" }) : play({ id: "off" });
+    toggleColorMode();
+  };
 
   return (
-    <IconButton
-      size="md"
-      fontSize="lg"
-      variant="ghost"
-      color="current"
-      marginLeft="2"
-      onClick={toggleColorMode}
-      icon={<SwitchIcon />}
-      aria-label={`Switch to ${text} mode`}
-      {...props}
-    />
-  )
-}
+    <Tooltip
+      label={text === "dark" ? "Dark mode" : "Light mode"}
+      aria-label="A tooltip"
+    >
+      <IconButton
+        size="md"
+        fontSize="md"
+        variant="ghost"
+        color="current"
+        marginLeft="2"
+        onClick={handleClick}
+        icon={<SwitchIcon />}
+        aria-label={`Switch to ${text} mode`}
+        _hover={{
+          bg: useColorModeValue("gray.200", "gray.900")
+        }}
+        {...props}
+      />
+    </Tooltip>
+  );
+};
