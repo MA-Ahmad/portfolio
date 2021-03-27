c640940cdc834fdd8c75f1ddaabe8a36e6dd4398
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-27 03:25:59 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from "react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import Header from "./components/header";
+import { theme } from "../src/style/theme";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+  <ChakraProvider theme={theme} resetCSS={true}>
+    <Header />
+    <Box
+      textAlign="center"
+      fontSize="xl"
+      w={["90%", "85%", "80%"]}
+      maxW={900}
+      mx="auto"
+    ></Box>
   </ChakraProvider>
-)
+);
