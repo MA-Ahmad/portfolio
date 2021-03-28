29f1c557bf8a6c15941acf7d60e0949cd4b68a64
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-28 10:06:38 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import * as React from "react";
 import { ChakraProvider, Box } from "@chakra-ui/react";
-import Header from "./components/header";
 import { theme } from "../src/style/theme";
+import Layout from "./layouts/layout";
 
 export const App = () => (
   <ChakraProvider theme={theme} resetCSS={true}>
-    <Header />
-    <Box
-      textAlign="center"
-      fontSize="xl"
-      w={["90%", "85%", "80%"]}
-      maxW={900}
-      mx="auto"
-    ></Box>
+    
+    <Layout />
   </ChakraProvider>
 );
