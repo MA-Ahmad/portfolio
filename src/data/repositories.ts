4994fd1edf4818e36dd495ca8ec0c0e20a794fe6
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-28 09:06:29 -0700

    Create repositories.ts

---
+++ src/data/repositories.ts
@@ -0,0 +1,80 @@
+import img1 from "../assets/images/cover_images/blog_1.png";
+import img2 from "../assets/images/cover_images/blog_2.png";
+import img3 from "../assets/images/cover_images/portfolio.png";
+import img4 from "../assets/images/cover_images/image_gallery.png";
+import img5 from "../assets/images/cover_images/notebook_app.png";
+import img6 from "../assets/images/cover_images/blog_3.png";
+
+export const repositories = [
+    {
+      title: 'Blog App(React)',
+      description:
+        'A React Blog App built by using libraries like ChakraUI, Formik.',
+      languages: 'Javascript, React, ChakraUi, Formik',
+      cover: img1,
+      url: 'https://github.com/MA-Ahmad/reactBlog',
+      live: 'https://ma-ahmad.github.io/reactBlog',
+      stars: '9',
+      fork: '1',
+      created: '21-4-2020'
+    },
+    {
+      title: 'Blog App (Front-end)',
+      description: 'React frontend of a blog app with rails on backend.',
+      cover: img2,
+      languages: 'Javascript, React, ChakraUi',
+      url: 'https://github.com/MA-Ahmad/blog-app-react-frontend',
+      live: 'https://blog-frontend-react.herokuapp.com',
+      stars: '6',
+      created: '21-4-2020'
+    },
+    {
+      title: 'Blog App (Back-end)',
+      description: 'Rails backend api for blog app with react on frontend.',
+      cover: img3,
+      languages: 'Ruby, Rails',
+      url: 'https://github.com/MA-Ahmad/blog-app-rails-backend',
+      stars: '2',
+      created: '21-4-2020'
+    },
+    {
+      title: 'Notebook',
+      description: 'Make notes for your daily work.',
+      cover: img4,
+      languages: 'Typescript, React, ChakraUi',
+      url: 'https://github.com/MA-Ahmad/notebook',
+      live: 'https://ma-ahmad.github.io/notebook',
+      stars: '2',
+      created: '21-4-2020'
+    },
+    {
+      title: 'Blog App',
+      description: 'A simple react+rails(RR) CRUD app with tailwindcss.',
+      cover: img5,
+      languages: 'Ruby, Rails, Javascript, React, TailwindCss',
+      url: 'https://github.com/MA-Ahmad/react_rails_blog',
+      live: 'https://react-on-rails-blog.herokuapp.com',
+      stars: '2',
+      created: '21-4-2020'
+    },
+    {
+      title: 'Authentication API',
+      description:
+        'Simple Authentication API that can be used with any front-end app like React, Vue, Angular app.',
+      cover: img6,
+      languages: 'Ruby, Rails',
+      url: 'https://github.com/MA-Ahmad/rails-authentication-app',
+      stars: '1',
+      created: '21-4-2020'
+    },
+    {
+      title: 'Video-to-text',
+      description: 'Convert a video to text using ruby.',
+      cover: img6,
+      languages: 'Ruby',
+      url: 'https://github.com/MA-Ahmad/Video-to-text',
+      stars: '1',
+      created: '21-4-2020'
+    }
+  ];
+  
