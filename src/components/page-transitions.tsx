a3783e92ca538a1535825b620b9182ee39dfaf8c
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-29 12:42:00 -0700

    Update page-transitions.tsx

--- src/components/page-transitions.tsx
+++ src/components/page-transitions.tsx
@@ -1,8 +1,59 @@
 import React from "react";
 import { SlideFade } from "@chakra-ui/react";
+import { MotionBox } from "./motion";
 
-const PageTransition = ({ children }) => {
+const staggerVariants = {
+  initial: {
+    opacity: 0,
+    translateY: -20
+  },
+  enter: {
+    transition: { staggerChildren: 0.15, delayChildren: 0.4 }
+  },
+  exit: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
+
+const variants = {
+  initial: {
+    opacity: 0,
+    translateY: -20
+  },
+  enter: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  exit: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
+
+export const PageSlideFade = ({ children }) => {
   return <SlideFade in>{children}</SlideFade>;
 };
 
-export default PageTransition;
+export const StaggerChildren = ({ children }) => {
+  return (
+    <MotionBox
+      animate={{
+        opacity: 1,
+        translateY: 0
+      }}
+      variants={staggerVariants}
+    >
+      {children}
+    </MotionBox>
+  );
+};
+
+export const CardTransition = ({ children }) => {
+  return <MotionBox variants={variants}>{children}</MotionBox>;
+};
