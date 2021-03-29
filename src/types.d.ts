7e00a4ea7c35790ad7038b48a45d622843696c2e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-29 06:25:26 -0700

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,3 +6,13 @@ type project = {
     desc: string;
     tech: string[];
   };
+
+  type article = {
+    title: string;
+    desc: string;
+    link: string;
+    views: string;
+    published: string;
+    tags: string[];
+    github_url: string;
+  }
