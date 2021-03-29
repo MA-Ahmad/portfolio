5aa3ead81fb1936178c68180f54211abca8e10cd
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-29 06:39:27 -0700

    Create layout.tsx

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import TopNav from "../components/top-nav";
+import Footer from "../components/footer";
+import Navigation from "../components/Navigation";
+
+const Layout = () => {
+  return (
+    <Router>
+      <TopNav />
+      <Box
+        textAlign="center"
+        fontSize="xl"
+        w={["90%", "85%", "80%"]}
+        maxW={800}
+        mx="auto"
+      >
+        <Box pt={10} pb={10}>
+          <Navigation />
+        </Box>
+      </Box>
+      <Footer />
+    </Router>
+  );
+};
+
+export default Layout;
