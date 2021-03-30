3c79d7cb4a344deac3772034081aa1d6632edb3e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-30 02:56:27 -0700

    Create data.ts

---
+++ src/data/data.ts
@@ -0,0 +1,51 @@
+import PhaedraLogo from "../assets/images/phaedra.jpeg";
+import BrainxLogo from "../assets/images/brainxtech.png";
+import CodenyaLogo from "../assets/images/codenya.png";
+import BrainplowLogo from "../assets/images/brainplow.jpg";
+import UniLogo from "../assets/images/fast.png";
+
+export const companies = [
+  {
+    title: "Phaedra Solutions",
+    alt: "PS image",
+    role: "Senior Software Engineer",
+    skills: ["ruby", "rails", "typescript", "javascript", "react", "aws"],
+    period: "2019 - Present",
+    logo: PhaedraLogo
+  },
+  {
+    title: "Brainx Technologies",
+    alt: "BT image",
+    role: "Software Engineer",
+    skills: ["ruby", "rails", "javascript", "python"],
+    period: "2018 - 2019",
+    logo: BrainxLogo
+  },
+  {
+    title: "Codenya Studio",
+    alt: "CS image",
+    role: "Co-founder, CTO",
+    skills: ["ruby", "rails", "python", "typescript", "react", "aws"],
+    period: "2018 - Present",
+    logo: CodenyaLogo
+  },
+  {
+    title: "Brainplow",
+    alt: "Brainplow image",
+    role: "Software Engineer",
+    skills: ["javascript", "python", "angular"],
+    period: "2017 - 2018",
+    logo: BrainplowLogo
+  }
+];
+
+export const institutes = [
+  {
+    title: "National University of Computer and Emerging Sciences",
+    alt: "Nuces image",
+    role: "Bachelor's Degree in Computer Science",
+    skills: ["project management", "web development", "data structures"],
+    period: "2013 - 2017",
+    logo: UniLogo,
+  }
+];
