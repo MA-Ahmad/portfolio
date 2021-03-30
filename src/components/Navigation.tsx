68562d3fb7f8e9b9f5c1f37515d1795b962bb287
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-30 05:16:18 -0700

    Create Navigation.tsx

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import About from "./about";
+import Blog from "./blog";
+import RepositoriesList from "./repositories-list";
+import Projects from "./projects";
+import Home from "./home";
+
+export interface NavigationProps {}
+const routes = [
+  { path: "/", exact: true, name: "Home", component: Home },
+  { path: "/about", exact: true, name: "About", component: About },
+  {
+    path: "/open-source",
+    exact: true,
+    name: "OpenSource",
+    component: RepositoriesList
+  },
+  { path: "/blog", exact: true, name: "Blog", component: Blog }
+];
+
+const Navigation: React.SFC<NavigationProps> = () => {
+  return (
+    <Switch>
+      {routes.map((route, idx) => (
+        <Route
+          exact={route.exact}
+          path={route.path}
+          render={props => <route.component {...props} />}
+        />
+      ))}
+      <Redirect to="/" />
+    </Switch>
+  );
+};
+
+export default Navigation;
