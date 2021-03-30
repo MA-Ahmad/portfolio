3bd5f078b47eaf5315aa247da30bd782b3c26aaa
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-30 05:58:31 -0700

    Create projects-list.ts

---
+++ src/data/projects-list.ts
@@ -0,0 +1,56 @@
+import peterpitch from "../assets/images/projects/pp.png";
+import notch from "../assets/images/projects/notch.png";
+import workof from "../assets/images/projects/workof.png";
+import souq from "../assets/images/projects/souq.png";
+import gosave from "../assets/images/projects/gosave.png";
+
+export const projectsList = [
+  {
+    id: 1,
+    title: "Peterpitch",
+    logo: peterpitch,
+    link: "https://peterpitch.com/",
+    desc: `Peterpitch is a tool that takes the fun in your company seriously
+          and turns your meetings into real performance boosters. With Peter
+          Pitch, you round up the pack of employees and turn even the shakiest
+          puppies into true wolves.`,
+    technologies: ["ruby", "rails", "react", "javascript"]
+  },
+  {
+    id: 2,
+    title: "Notch",
+    link: "https://www.hellonotch.com/",
+    logo: notch,
+    desc: `This is a project management website for design and procurement teams. 
+    It helps to collaborate and manage all of your floor plans in one place, writing specs, budgeting and timelines. 
+    This project management website provides a wide variety of features like timeline tracking, spec sheet generation, integrated budget, vendor + inventory management, user usage tracking and many more.`,
+    technologies: ["ruby", "rails", "javascript"]
+  },
+  {
+    id: 3,
+    title: "Workof Ecommerce",
+    logo: workof,
+    link: "https://workof.com/",
+    desc: `This is an Ecommerce furniture site. It provides a single platform to multiple vendors where they can put their products on sale.`,
+    technologies: ["ruby", "rails", "javascript"]
+  },
+  {
+    id: 4,
+    title: "Souq of Qatar",
+    logo: souq,
+    link: "https://www.souqofqatar.com/",
+    desc: `Souq of Qatar is online comparison website. This website is independent of any retailers, it displays the products of all the major stores.
+    Just like when you compare prices and products while visiting physical shops and stores, online price comparison behaves in the same way. 
+    It is an established method in the West for people to visit the online comparison websites first whenever they are in need to buy something. 
+    This provides the benefits of convenience, choice, and price efficiency.`,
+    technologies: ["ruby", "rails", "javascript"]
+  },
+  {
+    id: 5,
+    title: "GoSave",
+    logo: gosave,
+    link: "https://gosavehq.com/",
+    desc: `The GoSave app let's you send allowances, assign chores, and approve spending all from your smartphone. These actions appear on each kid's toy, along with a live bank balance so they can watch their savings grow.`,
+    technologies: ["ruby", "rails"]
+  }
+];
