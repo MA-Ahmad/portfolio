49e4a31fe1501448b04211350693f925bd5c149e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-01 10:55:41 -0700

    added motion image

--- src/components/motion.tsx
+++ src/components/motion.tsx
@@ -1,21 +1,44 @@
-import { Box, Flex, forwardRef } from '@chakra-ui/react';
-import { motion, isValidMotionProp } from 'framer-motion';
+import {
+  Box,
+  Flex,
+  Image,
+  Skeleton,
+  forwardRef,
+} from "@chakra-ui/react";
+import { motion, isValidMotionProp } from "framer-motion";
+import placeholder from "../assets/images/placeholder.png";
 
 export const MotionBox = motion.custom(
   forwardRef((props, ref) => {
     const chakraProps = Object.fromEntries(
       // do not pass framer props to DOM element
-      Object.entries(props).filter(([key]) => !isValidMotionProp(key)),
+      Object.entries(props).filter(([key]) => !isValidMotionProp(key))
     );
     return <Box ref={ref} {...chakraProps} />;
-  }),
+  })
 );
 export const MotionFlex = motion.custom(
   forwardRef((props, ref) => {
     const chakraProps = Object.fromEntries(
       // do not pass framer props to DOM element
-      Object.entries(props).filter(([key]) => !isValidMotionProp(key)),
+      Object.entries(props).filter(([key]) => !isValidMotionProp(key))
     );
     return <Flex ref={ref} {...chakraProps} />;
-  }),
+  })
+);
+
+export const MotionImage = motion.custom(
+  forwardRef((props, ref) => {
+    const chakraProps = Object.fromEntries(
+      // do not pass framer props to DOM element
+      Object.entries(props).filter(([key]) => !isValidMotionProp(key))
+    );
+    return (
+        <Image
+          ref={ref}
+          fallbackSrc={placeholder}
+          {...chakraProps}
+        />
+    );
+  })
 );
