87f655e1fa05d7490fdc7055e629365c6619ac44
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-01 10:36:08 -0700

    Create posts-list.ts

---
+++ src/data/posts/posts-list.ts
@@ -0,0 +1,21 @@
+import img1 from "../../assets/images/posts/notebook-app/img_1.png";
+import img2 from "../../assets/images/posts/notebook-app/img_2.png";
+import img3 from "../../assets/images/posts/notebook-app/img_3.png";
+import img4 from "../../assets/images/posts/notebook-app/img_4.png";
+import img5 from "../../assets/images/posts/notebook-app/img_5.png";
+import img6 from "../../assets/images/posts/notebook-app/img_5.png";
+
+export const posts = [
+  {
+    title: "Notebook-app",
+    desc:
+      "This is a notebook app which will help you to crate notes for your daily work.",
+    tags: ["react", "typescript", "chakraUi"],
+    url: "https://github.com/MA-Ahmad/notebook",
+    live: "https://ma-ahmad.github.io/notebook",
+    published: "1 April 2021",
+    readTime: "1 min read",
+    images: [img1, img2, img3, img4, img5, img6]
+    //   views: "229"
+  }
+];
