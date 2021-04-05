fca49a4de10d378df49286450d7a17d6b94ecfba
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-05 08:31:11 -0700

    Update repository-card.tsx

--- src/components/repository-card.tsx
+++ src/components/repository-card.tsx
@@ -24,51 +24,33 @@ import {
   Tooltip
 } from "@chakra-ui/react";
 import { MotionBox } from "./motion";
-import { getTypeColor } from "../style/theme";
-import { AiOutlineStar, AiOutlineShareAlt } from "react-icons/ai";
+import { getLanguageColor } from "../style/theme";
+import { AiOutlineStar } from "react-icons/ai";
+import { BiGitRepoForked, BiStar } from "react-icons/bi";
 import { FiGithub } from "react-icons/fi";
 import { motion } from "framer-motion";
 import { CardTransition } from "./page-transitions";
 import LazyImage from "./lazy-image";
-// import { format } from "timeago.js";
+
 interface RepositoryCardProps {
-  key: number;
   title: string;
   description: string;
-  cover: string;
-  blurHash: string;
-  technologies: string[];
   url: string;
-  live: string;
-  stars: string;
-  fork: string;
-  created: string;
+  language: string;
+  stargazers_count: number;
+  forks_count: number;
+  created_at: string;
 }
 const RepositoryCard = (props: RepositoryCardProps) => {
   const {
-    key,
     title,
     description,
-    cover,
-    blurHash,
-    technologies,
+    language,
     url,
-    live,
-    stars,
-    fork,
-    created
+    stargazers_count,
+    forks_count,
+    created_at
   } = props;
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleClick = () => {
-    onOpen();
-    // window.open(link);
-    // if (type == "link" || type == "article") {
-    //   window.open(link);
-    // } else {
-    //   onOpen();
-    // }
-  };
 
   const handleLinkClick = (
     e: React.MouseEvent<HTMLParagraphElement, MouseEvent>,
@@ -78,141 +60,77 @@ const RepositoryCard = (props: RepositoryCardProps) => {
     e.stopPropagation();
   };
 
-  const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
-
-  const thumbnailVariants = {
-    initial: { scale: 0.9, opacity: 0 },
-    enter: { scale: 1, opacity: 1, transition },
-    exit: {
-      scale: 0.5,
-      opacity: 0,
-      transition: { duration: 1.5, ...transition }
-    }
-  };
-
-  const imageVariants = {
-    hover: { scale: 1.1 }
-  };
-
   return (
-    <CardTransition>
-      <Box onClick={handleClick} cursor="pointer" size="xl">
-        <VStack
-          //   w="100%"
-          rounded="xl"
-          borderWidth="1px"
-          bg={useColorModeValue("white", "gray.800")}
-          borderColor={useColorModeValue("gray.100", "gray.700")}
-          _hover={{
-            shadow: "lg",
-            textDecoration: "none"
-          }}
-          overflow="hidden"
-          align="start"
-          spacing={0}
-        >
-          <Box position="relative" w="100%">
-            <MotionBox variants={thumbnailVariants}>
-              <MotionBox
-                whileHover="hover"
-                variants={imageVariants}
-                transition={transition}
-              >
-                <AspectRatio
-                  ratio={1.85 / 1}
-                  maxW="400px"
-                  w="100%"
-                  borderBottomWidth="1px"
-                  borderColor={useColorModeValue("gray.100", "gray.700")}
-                >
-                  {/* <Image
-                    src={cover}
-                    fallback={<Skeleton />}
-                    objectFit="cover"
-                  /> */}
-                  <LazyImage
-                    src={cover}
-                    blurHash={blurHash}
-                  />
-                </AspectRatio>
-              </MotionBox>
-            </MotionBox>
-          </Box>
-
-          <VStack py={2} px={[2, 4]} spacing={1} align="start" w="100%">
+    <MotionBox whileHover={{ y: -5 }}>
+      <Box
+        cursor="pointer"
+        size="xl"
+        py={2}
+        px={[2, 4]}
+        rounded="xl"
+        borderWidth="1px"
+        bg={useColorModeValue("white", "gray.800")}
+        borderColor={useColorModeValue("gray.100", "gray.700")}
+        _hover={{
+          shadow: "lg",
+          textDecoration: "none"
+        }}
+      >
+        <VStack overflow="hidden" align="start" spacing={1}>
+          <VStack spacing={1} align="start" w="100%">
             <Flex justifyContent={"space-between"} width="100%">
               <Tooltip hasArrow label="Github link" placement="top">
                 <HStack>
                   <Icon as={FiGithub} boxSize="0.9em" mt={"1px"} />
-                  {/* <Link href={url} isExternal> */}
                   <Text
                     fontSize="sm"
                     noOfLines={1}
                     fontWeight="600"
                     align="left"
+                    color={"blue.500"}
                     onClick={e => handleLinkClick(e, url)}
                   >
                     {title}
                   </Text>
                 </HStack>
               </Tooltip>
-              {/* </Link> */}
-              <Flex>
-                <Icon as={AiOutlineStar} boxSize="0.9em" mt={"1px"} />
-                <Box as="span" ml="1" fontSize="sm">
-                  {stars}
+              <HStack>
+                <Box>
+                  <Icon as={BiGitRepoForked} boxSize="0.9em" mt={"1px"} />
+                  <Box as="span" ml="1" fontSize="sm">
+                    {forks_count}
+                  </Box>
                 </Box>
-              </Flex>
+                <Box>
+                  <Icon as={BiStar} boxSize="0.9em" mt={"1px"} />
+                  <Box as="span" ml="1" fontSize="sm">
+                    {stargazers_count}
+                  </Box>
+                </Box>
+              </HStack>
             </Flex>
-            <Flex justifyContent={"space-between"} width="100%">
-              <Box>
-                <HStack spacing="1">
-                  {technologies.map(tech => (
-                    <Tag size="sm" colorScheme={getTypeColor(tech)}>
+            {language && (
+              <Flex justifyContent={"space-between"} width="100%">
+                <Box>
+                  <HStack spacing="1">
+                    <Tag size="sm" colorScheme={getLanguageColor(language)}>
                       <Text fontSize={["0.55rem", "inherit", "inherit"]}>
-                        {tech}
+                        {language}
                       </Text>
                     </Tag>
-                  ))}
-                </HStack>
-              </Box>
-            </Flex>
-            {/* <Flex justifyContent={"space-between"} width="100%">
-              <Flex>
-                <AiOutlineStar color="teal.300" />
-                <Box as="span" ml="1" fontSize="sm">
-                  {stars}
+                  </HStack>
                 </Box>
               </Flex>
-              <Box >
-              <Text
-                fontSize="xs"
-                fontWeight="400"
-                color={useColorModeValue("gray.400", "gray.500")}
-              >
-                {created}
-              </Text>
-            </Box>
-            </Flex> */}
+            )}
           </VStack>
+          <Box>
+            <Text color="gray.500" fontSize="sm" noOfLines={2} textAlign="left">
+              {description}
+            </Text>
+          </Box>{" "}
         </VStack>
-        <Modal isOpen={isOpen} onClose={onClose} isCentered allowPinchZoom>
-          <ModalOverlay />
-          <ModalContent bg="none" maxW={"28rem"} w="auto">
-            <ModalBody p={0} rounded="lg" overflow="hidden" bg="none">
-              <Center>
-                <Image src={cover} rounded="lg" />
-                {/* {type == "image" ? (
-                <Image src={cover} rounded="lg" />
-              ) : (
-                <ReactPlayer url={link} controls playing />
-              )} */}
-              </Center>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
       </Box>
-    </CardTransition>
+    </MotionBox>
   );
 };
 
