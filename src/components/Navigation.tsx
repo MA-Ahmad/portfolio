7c241f2b6004c7751d05efa7382e1402155c3cff
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-06 10:17:16 -0700

    added route for tech-stack

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,6 +4,7 @@ import About from "./about";
 import Posts from "./blog/posts";
 import RepositoriesList from "./repositories-list";
 import Home from "./home";
+import TechStack from "./tech-stack";
 import Achievements from "./achievements";
 import NotebookPost from "./blog/notebook-app/notebook-post";
 
@@ -28,7 +29,9 @@ const routes = [
     name: "OpenSource",
     component: RepositoriesList
   },
-  { path: "/blog", exact: true, name: "Blog", component: Posts }
+  { path: "/blog", exact: true, name: "Blog", component: Posts },
+  { path: "/tech-stack", exact: true, name: "Tools", component: TechStack }
+
 ];
 
 const Navigation = () => {
