93b48dc82fd0d5125a7a980e9041f7c3605becc1
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-09 13:20:35 -0700

    removed relative paths

--- src/components/blog/post-card.tsx
+++ src/components/blog/post-card.tsx
@@ -16,10 +16,9 @@ import {
 } from "@chakra-ui/react";
 import { Link as NavLink } from "react-router-dom";
 import { FaEye } from "react-icons/fa";
-import { MotionBox } from "../motion";
-import { getTagColor } from "../../style/theme";
-import dev from "../../assets/images/logos/dev.png";
-import dev2 from "../../assets/images/logos/dev_white.png";
+import { getTagColor } from "style/theme";
+import dev from "assets/images/logos/dev.png";
+import dev2 from "assets/images/logos/dev_white.png";
 import { CardTransition } from "../page-transitions";
 
 export interface PostCardProps {
