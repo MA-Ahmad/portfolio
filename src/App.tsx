630b05a438ff5487406a0579dd9f07c1df60670d
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-09 13:38:17 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { ChakraProvider, Box } from "@chakra-ui/react";
-import { theme } from "../src/style/theme";
+import { theme } from "style/theme";
 import Layout from "./layouts/layout";
 
 export const App = () => (
   <ChakraProvider theme={theme} resetCSS={true}>
-    
     <Layout />
   </ChakraProvider>
 );
