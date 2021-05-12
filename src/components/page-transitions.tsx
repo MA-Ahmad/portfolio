c00ae90a392a16f44c4d706659428d4730bc8ffc
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-12 10:23:28 -0700

    added container and item variants

--- src/components/page-transitions.tsx
+++ src/components/page-transitions.tsx
@@ -36,6 +36,27 @@ const variants = {
   }
 };
 
+
+export const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+export const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
+
 export const PageSlideFade = ({ children }) => {
   return <SlideFade in>{children}</SlideFade>;
 };
