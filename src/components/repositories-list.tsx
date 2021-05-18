250db829ae4a082fa459cb2d857bc97dce1bf7cc
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-18 12:36:32 -0700

    Update repositories-list.tsx

--- src/components/repositories-list.tsx
+++ src/components/repositories-list.tsx
@@ -1,55 +1,61 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
-import useFetch from "use-http";
 import {
   VStack,
   Text,
   useColorModeValue,
-  SimpleGrid,
-  Box,
-  useBoolean,
-  useMediaQuery
+  HStack,
+  IconButton,
+  Tooltip
 } from "@chakra-ui/react";
-import { PageSlideFade, StaggerChildren } from "./page-transitions";
-import RepositoryCard from "./repository-card";
-import StackGrid from "react-stack-grid";
+import { PageSlideFade } from "./page-transitions";
 import Header from "./header";
-import { MotionBox } from "./motion";
-import CardSkeleton from "./card-skeleton";
+import { RiSignalTowerLine, RiWifiOffLine } from "react-icons/ri";
+import OfflineData from "./offline-data";
+import LiveData from "./live-data";
 
 const TURQUOISE = "#06b6d4";
 
+const iconProps = {
+  variant: "ghost",
+  size: "md",
+  isRound: true
+};
+
 const RepositoriesList = () => {
-  const { get, loading, error, data } = useFetch("https://api.github.com");
-  const [repos, setRepos] = useState([]);
-  const [isLargerThan720] = useMediaQuery("(min-width: 720px)");
-  const [isLargerThan982] = useMediaQuery("(min-width: 982px)");
-
-  let columnWidth = 390;
-  if (isLargerThan982) {
-    columnWidth = 390;
-  } else {
-    if (isLargerThan720) {
-      columnWidth = 300;
-    } else {
-      columnWidth = "100%";
-    }
-  }
-
-  useEffect(() => {
-    get("/users/MA-Ahmad/repos").then(res => {
-      setRepos(
-        res?.sort((a, b) => b.stargazers_count - a.stargazers_count).slice(0, 8)
-      );
-    });
-  }, []);
+  const [activeTab, setActiveTab] = React.useState("live");
 
   return (
     <PageSlideFade>
       <VStack align="start" spacing={3}>
-        <Header underlineColor={TURQUOISE} mt={0} mb={0}>
-          Open Source
-        </Header>
+        <HStack justifyContent={"space-between"} width={"100%"}>
+          <Header underlineColor={TURQUOISE} mt={0} mb={0}>
+            Open Source
+          </Header>
+          <HStack>
+          <Tooltip hasArrow label="Live github data" placement="top">
+            <IconButton
+              aria-label={"live"}
+              size="md"
+              colorScheme={"linkedin"}
+              icon={<RiSignalTowerLine />}
+              isActive={activeTab === "live"}
+              onClick={() => setActiveTab("live")}
+              {...iconProps}
+            />
+            </Tooltip>
+          <Tooltip hasArrow label="Offline github data" placement="top">
+            <IconButton
+              aria-label={"live"}
+              size="md"
+              colorScheme={"linkedin"}
+              icon={<RiWifiOffLine />}
+              isActive={activeTab === "offline"}
+              onClick={() => setActiveTab("offline")}
+              {...iconProps}
+            />
+            </Tooltip>
+          </HStack>
+        </HStack>
         <Text
           color={useColorModeValue("gray.500", "gray.200")}
           textAlign="left"
@@ -58,27 +64,7 @@ const RepositoriesList = () => {
           or contributed to.
         </Text>
       </VStack>
-      {loading ? (
-        <SimpleGrid columns={[1, 1, 2]} spacing={4} mt={4}>
-          <CardSkeleton />
-        </SimpleGrid>
-      ) : (
-        <Box mt={4}>
-          <StackGrid columnWidth={columnWidth}>
-            {repos?.map((repo, index) => (
-              <RepositoryCard
-                title={repo.name}
-                description={repo.description}
-                language={repo.language}
-                url={repo.svn_url}
-                created_at={repo.created_at}
-                stargazers_count={repo.stargazers_count}
-                forks_count={repo.forks_count}
-              />
-            ))}
-          </StackGrid>
-        </Box>
-      )}
+      {activeTab === "live" ? <LiveData /> : <OfflineData />}
     </PageSlideFade>
   );
 };
