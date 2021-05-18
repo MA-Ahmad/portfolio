19ea62f32b9b6d0abdabe326a906d6720a76e9f3
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-18 13:25:46 -0700

    removed commented code

--- src/components/repositories-list.tsx
+++ src/components/repositories-list.tsx
@@ -66,7 +66,6 @@ const RepositoriesList = () => {
               icon={<RiWifiOffLine />}
               isActive={activeTab === "offline"}
               onClick={() => handleClick('offline')}
-              // onClick={() => setActiveTab("offline")}
               {...iconProps}
             />
             </Tooltip>
