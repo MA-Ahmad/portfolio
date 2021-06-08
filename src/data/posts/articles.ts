96e68767ff70850d7e1bab6dc9bbf9fe3af18db9
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-08 11:03:45 -0700

    Update articles.ts

--- src/data/posts/articles.ts
+++ src/data/posts/articles.ts
@@ -14,7 +14,7 @@ export const articles = [
     live: "https://github.com/MA-Ahmad/portfolio",
     published: "3 Apr 2021",
     readTime: "1 min read",
-    views: "1424"
+    views: "1472"
   },
   {
     title: "Create a Notebook app with React, CharaUi, and framerMotion",
@@ -29,7 +29,7 @@ export const articles = [
     published: "2 Apr 2021",
     readTime: "1 min read",
     images: images,
-    views: "2189"
+    views: "2210"
   },
   {
     title:
