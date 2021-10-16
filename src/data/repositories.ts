eb4a70c4ac93ffb70cecb124b1c0b0ae7627c587
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-10-16 09:38:33 -0700

    update repositories

--- src/data/repositories.ts
+++ src/data/repositories.ts
@@ -139,22 +139,22 @@ export const repositories = [
   //   technologies: ["ruby"],
   //   url: "https://github.com/MA-Ahmad/Video-to-text",
   //   blurHash: "L4S~x5xb~q$*~WR6MykCx^Vt4TtP",
-  //   stars: "1"
-  // }
-  // {
-  //   title: "Authentication API",
-  //   description:
-  //     "Simple Authentication API that can be used with any front-end app like React, Vue, Angular app.",
-  //   technologies: ["ruby", "rails"],
-  //   url: "https://github.com/MA-Ahmad/rails-authentication-app",
-  //   blurHash: "L4S~x5xb~q$*~WR6MykCx^Vt4TtP",
-  //   stars: "1"
+  //   stars: "3"
   // },
   // {
   //   title: "Blog App (back-end)",
   //   description: "Rails backend api for blog app with react on frontend.",
   //   technologies: ["ruby", "rails"],
   //   url: "https://github.com/MA-Ahmad/blog-app-rails-backend",
+  //   blurHash: "L4S~x5xb~q$*~WR6MykCx^Vt4TtP",
+  //   stars: "3"
+  // },
+  // {
+  //   title: "Authentication API",
+  //   description:
+  //     "Simple Authentication API that can be used with any front-end app like React, Vue, Angular app.",
+  //   technologies: ["ruby", "rails"],
+  //   url: "https://github.com/MA-Ahmad/rails-authentication-app",
   //   blurHash: "L4S~x5xb~q$*~WR6MykCx^Vt4TtP",
   //   stars: "2"
   // }
