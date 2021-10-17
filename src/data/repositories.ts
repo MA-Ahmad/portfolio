02e46b208af4b170c71d303e3b93f4ebd8fb4ccb
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-10-17 04:30:34 -0700

    update repositories

--- src/data/repositories.ts
+++ src/data/repositories.ts
@@ -133,19 +133,19 @@ export const repositories = [
     stars: "2"
   }
   // {
+  //   title: "Blog App (back-end)",
+  //   description: "Rails backend api for blog app with react on frontend.",
+  //   technologies: ["ruby", "rails"],
+  //   url: "https://github.com/MA-Ahmad/blog-app-rails-backend",
+  //   blurHash: "L4S~x5xb~q$*~WR6MykCx^Vt4TtP",
+  //   stars: "3"
+  // },
+  // {
   //   title: "Video-to-text",
   //   description: "Convert a video to text using ruby.",
   //   cover: "",
   //   technologies: ["ruby"],
   //   url: "https://github.com/MA-Ahmad/Video-to-text",
-  //   blurHash: "L4S~x5xb~q$*~WR6MykCx^Vt4TtP",
-  //   stars: "3"
-  // },
-  // {
-  //   title: "Blog App (back-end)",
-  //   description: "Rails backend api for blog app with react on frontend.",
-  //   technologies: ["ruby", "rails"],
-  //   url: "https://github.com/MA-Ahmad/blog-app-rails-backend",
   //   blurHash: "L4S~x5xb~q$*~WR6MykCx^Vt4TtP",
   //   stars: "3"
   // },
