6f4e8bc8ebf2e7ca2a4ca085c09ce1b839aeef4b
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-01-01 01:20:53 +0500

    Update repositories.ts

--- src/data/repositories.ts
+++ src/data/repositories.ts
@@ -19,7 +19,7 @@ export const repositories = [
     live: "https://mahmad.me",
     blurHash: "L25#he^nryxc^-w$V{V_56bqx[M{",
     stars: "64",
-    fork: "11"
+    fork: "12"
   },
   {
     title: "Portfolio2",
